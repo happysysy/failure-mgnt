38d508feeb50c3ebf77fb40fd869fb4b944665ec
happysysy <happy-sysy@hanmail.net>
2020-05-18 18:40:25 +0900

    Initial commit

---
+++ src/main/java/com/cntt/systemfailure/springboot/Application.java
@@ -0,0 +1,24 @@
+package com.cntt.systemfailure.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main( String[] args) {
+
+        SpringApplication.run( Application.class, args );
+    }
+
+    @Bean
+    public InternalResourceViewResolver setupViewResolver() {
+
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix( "/WEB-INF/view/" );
+        resolver.setSuffix( ".html" );
+        return resolver;
+    }
+}
---
+++ src/main/java/com/cntt/systemfailure/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,8 @@
+package com.cntt.systemfailure.springboot.domain.posts;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+}
